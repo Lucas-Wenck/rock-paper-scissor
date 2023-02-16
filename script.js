2a9b147f83f373092fd7fd53647db565635387c3
Lucas Wenck <lucas.wenck@gmail.com>
2023-02-16 03:14:00 -0300

    Finish round function with if conditionals and statements

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 const options = [`Rock`, `Paper`, `Scissors`]
+let compChoice;
 
-function getComputerChoice(compChoice){
+function getComputerChoice(){
     compChoice = Math.floor(Math.random() * options.length);
-    return options[compChoice];
+    return compChoice = options[compChoice];
 }
 
-function round(computerSelection, playerSelection){
+function round(){
     let player = prompt(`Please choose rock paper or scissors`)
     player = player.trim();
     player = player.charAt(0).toUpperCase() + player.slice(1).toLowerCase();
-    console.log(player);
+    if(player != `Rock` && player != `Paper` && player != `Scissors`){
+        return `Thats not an option`
+    }
+    else{
+        getComputerChoice();
+        if((player == `Rock` && compChoice == `Scissors`) || (player == `Paper` && compChoice == `Rock`) || (player == `Scissors` && compChoice == `Paper`)){
+            return `Congratulations! ${player} beats ${compChoice}!`;
+        }
+        else if((player == `Rock` && compChoice == `Paper`) || (player == `Paper` && compChoice == `Scissors`) || (player == `Scissors` && compChoice == `Rock`)){
+            return `Sorry! ${compChoice} beats ${player}.`;
+        }
+        else{
+            console.log(player)
+            console.log(compChoice)
+            return `A draw!`
+        }
+    }
 }
