1618aa458e9a4e590014c84332df882af4281d0f
Lucas Wenck <lucas.wenck@gmail.com>
2023-02-16 02:43:12 -0300

    Create beginning of round function
    Create beginning of round function where the player writes his choice
    and them the choice is stripped of white space and capitalized correctly

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function getComputerChoice(compChoice){
     compChoice = Math.floor(Math.random() * options.length);
     return options[compChoice];
 }
+
+function round(computerSelection, playerSelection){
+    let player = prompt(`Please choose rock paper or scissors`)
+    player = player.trim();
+    player = player.charAt(0).toUpperCase() + player.slice(1).toLowerCase();
+    console.log(player);
+}
