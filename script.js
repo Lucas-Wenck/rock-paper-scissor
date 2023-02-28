07118cf71907d4df3d34fc51fe4596f37e1212ac
Lucas Wenck <lucas.wenck@gmail.com>
2023-02-28 02:32:24 -0300

    Add best of 5 to the game

--- script.js
+++ script.js
@@ -14,14 +14,30 @@ function round(){
         playerVicCount = playerVicCount + 1;
         this.document.getElementById('talk').innerText = `Congrats! ${player} beats ${compChoice}!`;
         this.document.getElementById('playerScore').innerText = `${playerVicCount}`;
+        this.document.getElementById('compScore').innerText = `${compVicCount}`;
+        if(playerVicCount == `5`){
+            window.alert("Wow! You Win!");
+            this.document.getElementById('talk').innerText = `Congrats on winning!`;
+            playerVicCount = 0;
+            compVicCount = 0;        
+        };
     }
     else if((player == `Rock` && compChoice == `Paper`) || (player == `Paper` && compChoice == `Scissors`) || (player == `Scissors` && compChoice == `Rock`)){
         compVicCount = compVicCount + 1;
         this.document.getElementById('talk').innerText = `Sorry! ${compChoice} beats ${player}.`;
         this.document.getElementById('compScore').innerText = `${compVicCount}`;
+        this.document.getElementById('playerScore').innerText = `${playerVicCount}`;
+        if(compVicCount == `5`){
+            window.alert("Sorry! Better luck next time!");
+            this.document.getElementById('talk').innerText = `Better luck next time friend!`;
+            playerVicCount = 0;
+            compVicCount = 0;        
+        };
     }
     else{
         this.document.getElementById('talk').innerText = `A draw!`;
+        this.document.getElementById('playerScore').innerText = `${playerVicCount}`;
+        this.document.getElementById('compScore').innerText = `${compVicCount}`;
     }
 }
 const rock = document.querySelector('#rock');
